2275e97af7946551c2b3856f910d0e2c7a73b9a2
Anastasia Susnicova <nastasushnicova04@mail.ru>
2022-11-19 21:33:38 +0300

    Урок12

---
+++ Урок 12 Сушникова.sql
@@ -0,0 +1,6 @@
+/* Задание 7.1 */
+select substring('Please find the substring in this string', 17, 9);
+/* Задание 7.2 */
+select abs(-25.76823), sign(-25.76823), round(-25.76823, 2);
+/* Задание 7.3 */
+select extract(month from current_date());
